f63fdfab06e21decc27bdc45d12095696a942084
Hillary X <hillaryx@hillarys-air.mynetworksettings.com>
2024-05-26 22:12:07 -0400

    Implement Routers

--- server/src/routes/fin-records.ts
+++ server/src/routes/fin-records.ts
@@ -11,12 +11,50 @@ router.get('/getAllByUserID/:userId', async (req: Request, res: Response) => {
         return res.status(404).send("No records found for the user.")
       }
       res.status(200).send(records);
-
-    } catch (err) {
+      } catch (err) {
       res.status(500).send(err); 
     }
 });
 
+router.post('/', async (req: Request, res: Response) => {
+  try {
+    const newRecordBody = req.body
+    const newRecord = new FinRecordModel(newRecordBody)
+    const savedRecord = await newRecord.save();
+    
+    res.status(200).send(savedRecord);
+    } catch (err) {
+    res.status(500).send(err); 
+  }
+});
 
+router.put('/id:', async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const newRecordBody = req.body;
+   const record = await FinRecordModel.findByIdAndUpdate(
+    id, 
+    newRecordBody,
+    { new: true }
+  );
+
+    if(!record) return res.status(404).send();
+    
+        res.status(200).send(record);
+    } catch (err) {
+    res.status(500).send(err); 
+  }
+});
+
+router.delete('/id:', async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const record = await FinRecordModel.findByIdAndDelete(id);
+    if(!record) return res.status(404).send();
+        res.status(200).send(record);
+    } catch (err) {
+    res.status(500).send(err); 
+  }
+});
 
 export default router;
